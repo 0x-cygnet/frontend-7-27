b40ab94c91fa5007a552c49d4b16e4fb5f293926
p00gz <bloopsocket@gmail.com>
2021-07-27 18:46:56 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>I was changed! Modified-27Jul21- by bob</p>
+        <p>I was changed! Modified</p>
         <a
           className="App-link"
           href="https://reactjs.org"
